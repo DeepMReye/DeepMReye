ee6ddb6e5d5728b266a5b4acb3e5e6aa60307a66
CYHSM <markus.frey1@gmail.com>
2021-09-23 16:11:09 +0200

    Renamed scripts and added combined training script

--- scripts/train_combined.py
+++ scripts/train_combined.py
@@ -10,7 +10,7 @@ from deepmreye.util import data_generator, model_opts
 from deepmreye.util.util import CLI_OPTIONS
 
 # --------------------------------------------------------------------------------
-# ------------------------Evaluate model on given dataset-------------------------
+# ------------------------Evaluate model on all datasets--------------------------
 # --------------------------------------------------------------------------------
 
 # Parse input arguments
@@ -18,17 +18,24 @@ parser = ArgumentParser()
 for val, item in CLI_OPTIONS.items():
     parser.add_argument(*item.cli, dest=val, **item.kwargs)
 args = parser.parse_args()
+
 # Set GPU
 os.environ["CUDA_VISIBLE_DEVICES"] = "{}".format(args.gpu_id)
 
 # Create data generators from given path
-dataset_name = os.path.basename(os.path.dirname(args.dataset_path))
-generators = data_generator.create_holdout_generators([args.dataset_path], train_split=0.90)
+parsed_datasets = args.datasets.split(',')
+if 'jbj' in parsed_datasets:
+    parsed_datasets = ['jbj'] + parsed_datasets
+else:
+    parsed_datasets = parsed_datasets[0] + parsed_datasets
+datasets = [args.dataset_path + ds + os.path.sep for ds in parsed_datasets]
 opts = model_opts.get_opts()
+generators = data_generator.create_leaveoneout_generators(datasets, batch_size=opts['batch_size'], augment_list=((opts['rotation_x'], opts['rotation_y'], opts['rotation_z']), opts['shift'], opts['zoom']), mixed_batches=True)
+
 # User options which are different than the default are adjusted *within* the script (uncomment line below) not at command line
-# opts['epochs'] = 15
+opts['epochs'] = 50
 
 # Train model and save weights into weights_path
-(model, model_inference) = train.train_model(dataset=dataset_name, generators=generators, opts=opts, use_multiprocessing=True,
+(model, model_inference) = train.train_model(dataset='_'.join(parsed_datasets), generators=generators[0], opts=opts, use_multiprocessing=True,
                                              return_untrained=False, verbose=1, save=True, model_path=args.weights_path)
 
---
+++ scripts/train_holdout.py
@@ -0,0 +1,36 @@
+# System imports
+import sys
+sys.path.insert(0, "/home/marx/Documents/Github/DeepMReye")
+import os
+from argparse import ArgumentParser
+
+# DeepMReye imports
+from deepmreye import architecture, train, analyse, preprocess
+from deepmreye.util import data_generator, model_opts
+from deepmreye.util.util import CLI_OPTIONS
+
+# --------------------------------------------------------------------------------
+# ------------------------Evaluate model on given dataset-------------------------
+# --------------------------------------------------------------------------------
+
+# Parse input arguments
+parser = ArgumentParser()
+for val, item in CLI_OPTIONS.items():
+    parser.add_argument(*item.cli, dest=val, **item.kwargs)
+args = parser.parse_args()
+
+# Set GPU
+os.environ["CUDA_VISIBLE_DEVICES"] = "{}".format(args.gpu_id)
+
+# Create data generators from given path
+dataset_name = os.path.basename(os.path.dirname(args.dataset_path))
+opts = model_opts.get_opts()
+generators = data_generator.create_holdout_generators([args.dataset_path], train_split=0.90, batch_size=opts['batch_size'], augment_list=((opts['rotation_x'], opts['rotation_y'], opts['rotation_z']), opts['shift'], opts['zoom']), mixed_batches=True)
+
+# User options which are different than the default are adjusted *within* the script (uncomment line below) not at command line
+# opts['epochs'] = 15
+
+# Train model and save weights into weights_path
+(model, model_inference) = train.train_model(dataset=dataset_name, generators=generators, opts=opts, use_multiprocessing=True,
+                                             return_untrained=False, verbose=1, save=True, model_path=args.weights_path)
+
