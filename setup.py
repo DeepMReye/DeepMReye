5d7b4ae084a02645e3f17a11e4a8056dffbe9f66
Remi Gau <remi_gau@hotmail.com>
2022-10-20 11:22:50 +0200

    update metadata and add packaging infra

--- setup.py
+++ setup.py
@@ -14,6 +14,4 @@ SETUP_REQUIRES = ['setuptools >= 30.3.0']
 SETUP_REQUIRES += ['wheel'] if 'bdist_wheel' in sys.argv else []
 
 if __name__ == "__main__":
-    setup(name='deepmreye',
-          setup_requires=SETUP_REQUIRES,
-          version='0.1')
+    setup(setup_requires=SETUP_REQUIRES)
