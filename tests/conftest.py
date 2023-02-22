c5b3ca18ccc864448cb8de0586c062761bc60d38
CYHSM <markus.frey1@gmail.com>
2023-02-22 11:11:13 +0100

    Fixed path in conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,10 +4,10 @@ from pathlib import Path
 
 @pytest.fixture
 def path_to_masks():
-    path = str(Path(__file__).parents[0]) + os.path.sep + 'data/'
+    path = str(Path(__file__).parents[0].parents[0]) + os.path.sep + 'deepmreye/masks/'
     return path
 
 @pytest.fixture
 def path_to_testdata():
-    path = str(Path(__file__).parents[0]) + os.path.sep + 'data/'
+    path = str(Path(__file__).parents[0].parents[0]) + os.path.sep + 'deepmreye/masks/'
     return path
